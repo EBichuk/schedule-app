ab03768c92326ea75d86fdb0d63a16a1187be4c0
EBichuk <bi4uk-Katia@yandex.ru>
2025-05-12 06:03:31 +0200

    move homework to dir hw

--- Makefile
+++
@@ -1,5 +0,0 @@
-gen:
-	oapi-codegen -generate types -package api docs/openapi.yaml > pkg/types/types.gen.go
-
-protogen:
-	protoc -I proto proto/schedule.proto --go_out=./proto/gen/ --go_opt=paths=source_relative --go-grpc_out=./proto/gen/ --go-grpc_opt=paths=source_relative
--- inserte_into.sql
+++
@@ -1,8 +0,0 @@
-insert into developers(id, name, department, geolocation, last_known_ip, is_available)
-select	uuid_generate_v4(),
-	    (array['James', 'Mary', 'John', 'Patricia', 'Robert'])[floor(random() * 5+1)] || ' ' || (array['Smith', 'Johnson', 'Williams', 'Brown', 'Jones'])[floor(random() * 4+1)], 
-	    (array['backend', 'android', 'ios', 'frontend'])[floor(random() * 4+1)],
-	    st_setsrid(st_makepoint(54.713505 + random() * 5, 20.454683 + random() * 5), 4326), 
-	    (floor(random() * 255)::text || '.' || floor(random() * 255)::text || '.' || floor(random() * 255)::text ||'.' || floor(random() * 255)::text)::cidr,
-	    (array[true, false])[floor(random() * 2+1)]
-from generate_series(1, 100000)
