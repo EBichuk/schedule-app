62293506fc60841d7ddc8a7eb4cb37c6b77b22eb
EBichuk <bi4uk-Katia@yandex.ru>
2025-05-12 06:06:50 +0200

    add testdata for integration testing

---
+++ tests/testdata/create_schedule.sql
@@ -0,0 +1,12 @@
+INSERT INTO schedules
+(id, user_id, name_medication, medication_per_day, duration_medication)
+VALUES(1, 1, 'Ромашка', 10, 14);
+INSERT INTO schedules
+(id, user_id, name_medication, medication_per_day, duration_medication)
+VALUES(2, 1, 'Ром', 6, 2);
+INSERT INTO schedules
+(id, user_id, name_medication, medication_per_day, duration_medication)
+VALUES(3, 3, 'Грамедин Плюс', 6, 2);
+INSERT INTO schedules
+(id, user_id, name_medication, medication_per_day, duration_medication)
+VALUES(4, 2, 'Сорбифер Дурулес', 3, 30);
---
+++ tests/testdata/get_schedule.sql
@@ -0,0 +1,27 @@
+INSERT INTO schedules
+(id, user_id, name_medication, medication_per_day, duration_medication)
+VALUES(1, 1, 'Ромашка', 10, 14);
+INSERT INTO schedules
+(id, user_id, name_medication, medication_per_day, duration_medication)
+VALUES(2, 1, 'Ром', 6, 2);
+INSERT INTO schedules
+(id, user_id, name_medication, medication_per_day, duration_medication)
+VALUES(3, 3, 'Грамедин Плюс', 6, 2);
+INSERT INTO schedules
+(id, user_id, name_medication, medication_per_day, duration_medication)
+VALUES(4, 1, 'Фарингосепт', 1, 14);
+INSERT INTO schedules
+(id, user_id, name_medication, medication_per_day, duration_medication)
+VALUES(5, 1, 'Цикловир', 1, 14);
+INSERT INTO schedules
+(id, user_id, name_medication, medication_per_day, duration_medication)
+VALUES(6, 1, 'Укатирол', 1, 14);
+INSERT INTO schedules
+(id, user_id, name_medication, medication_per_day, duration_medication)
+VALUES(7, 1, 'Фердуцин', 1, 14);
+INSERT INTO schedules
+(id, user_id, name_medication, medication_per_day, duration_medication)
+VALUES(8, 1, 'Сорбифер Дурулес', 1, 14);
+INSERT INTO schedules
+(id, user_id, name_medication, medication_per_day, duration_medication)
+VALUES(9, 1, 'Нурофен', 4, 14);
